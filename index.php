23fecd7a099a427779648dd9ef38587db89ff74d
marcocentonze <marco.centonze27@gmail.com>
2023-10-09 15:50:56 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="script.php" method="POST">
+    <input type="text" name="randomtext" id="randomtext" placeholder="Type your text">
+    <input type="password" name="">
+   
+
+    <button type="submit">Save</button>
+  </form>
+
+
+
+</body>
+</html>
